a34d234992df3c2a58dd7808d040af80bd949bac
bernal9913 <ramirez.bernal.jesus1@gmail.com>
2023-05-09 22:38:42 -0700

    fix de login

--- src/page/Login.js
+++ src/page/Login.js
@@ -11,13 +11,9 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     //usamos el login del context
-<<<<<<< HEAD
-    const { login} = useAuth();
      const [error, setError] = useState("");
-=======
     const { login } = useAuth();
     //const [error, setError] = useState("");
->>>>>>> aab2b88fc9ea2d957a7c0acd0b0e4e21685024b6
 
     const onLogin = (e) => {
         setError("");
@@ -42,52 +38,6 @@ const Login = () => {
     }
 
     return (
-<<<<<<< HEAD
-      <>
-        <main>
-          <section>
-            <div>
-              <p> FocusApp </p>
-              <form>
-                <div>
-                  <label htmlFor="email-address">Email address</label>
-                  <input
-                    id="email-address"
-                    name="email"
-                    type="email"
-                    required
-                    placeholder="Email address"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="password">Password</label>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-
-                <div>
-                  <button onClick={onLogin}>Login</button>
-                </div>
-              </form>
-              {error && <p>{error}</p>}
-
-              <p className="text-sm text-white text-center">
-                No account yet? <NavLink to="/signup">Sign up</NavLink>
-              </p>
-            </div>
-          </section>
-        </main>
-      </>
-    );
-=======
         <>
             <main>
                 <section>
@@ -97,29 +47,29 @@ const Login = () => {
                         <form>
                             <div className='mb-3'>
                                 <input className='form-control'
-                                    id="email-address"
-                                    name="email"
-                                    type="email"
-                                    required
-                                    placeholder="Correo"
-                                    onChange={(e) => setEmail(e.target.value)}
+                                       id="email-address"
+                                       name="email"
+                                       type="email"
+                                       required
+                                       placeholder="Correo"
+                                       onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
 
                             <div className='mb-3'>
                                 <input className='form-control'
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    placeholder="Contraseña"
-                                    onChange={(e) => setPassword(e.target.value)}
+                                       id="password"
+                                       name="password"
+                                       type="password"
+                                       required
+                                       placeholder="Contraseña"
+                                       onChange={(e) => setPassword(e.target.value)}
                                 />
                             </div>
 
                             <div className='d-grid'>
                                 <button type='button' class='btn btn-primary btn-block'
-                                    onClick={onLogin}
+                                        onClick={onLogin}
                                 >
                                     Iniciar sesión
                                 </button>
@@ -130,7 +80,6 @@ const Login = () => {
             </main>
         </>
     )
->>>>>>> aab2b88fc9ea2d957a7c0acd0b0e4e21685024b6
 }
 
 export default Login
