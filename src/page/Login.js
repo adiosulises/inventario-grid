5947bd5143fd6009c17f9707c6db31b0113637bd
alexander chavez <alexanderchavez264@gmail.com>
2023-05-09 20:57:57 -0700

    mostrar el mensaje de error

--- src/page/Login.js
+++ src/page/Login.js
@@ -11,9 +11,10 @@ const Login = () => {
     const [password, setPassword] = useState('');
     //usamos el login del context
     const { login} = useAuth();
-     //const [error, setError] = useState("");
+     const [error, setError] = useState("");
 
     const onLogin = (e) => {
+        setError("");
         e.preventDefault();
              login(email,password)
             .then((userCredential) => {
@@ -21,72 +22,64 @@ const Login = () => {
                 const user = userCredential.user;
                 navigate("/home")
                 console.log(user);
+                
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 console.log(errorCode, errorMessage)
+                setError(errorMessage)
             });
-
+        
+        
+      
     }
 
-    return(
-        <>
-            <main >
-                <section>
-                    <div>
-                        <p> FocusApp </p>
+    return (
+      <>
+        <main>
+          <section>
+            <div>
+              <p> FocusApp </p>
+              <form>
+                <div>
+                  <label htmlFor="email-address">Email address</label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    required
+                    placeholder="Email address"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
 
-                        <form>
-                            <div>
-                                <label htmlFor="email-address">
-                                    Email address
-                                </label>
-                                <input
-                                    id="email-address"
-                                    name="email"
-                                    type="email"
-                                    required
-                                    placeholder="Email address"
-                                    onChange={(e)=>setEmail(e.target.value)}
-                                />
-                            </div>
+                <div>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
 
-                            <div>
-                                <label htmlFor="password">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    placeholder="Password"
-                                    onChange={(e)=>setPassword(e.target.value)}
-                                />
-                            </div>
+                <div>
+                  <button onClick={onLogin}>Login</button>
+                </div>
+              </form>
+              {error && <p>{error}</p>}
 
-                            <div>
-                                <button
-                                    onClick={onLogin}
-                                >
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-
-                        <p className="text-sm text-white text-center">
-                            No account yet? {' '}
-                            <NavLink to="/signup">
-                                Sign up
-                            </NavLink>
-                        </p>
-
-                    </div>
-                </section>
-            </main>
-        </>
-    )
+              <p className="text-sm text-white text-center">
+                No account yet? <NavLink to="/signup">Sign up</NavLink>
+              </p>
+            </div>
+          </section>
+        </main>
+      </>
+    );
 }
 
 export default Login
