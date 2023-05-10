87ceece7c9c0fd66fae4e052a282b95fbd581057
adiosulises <68453081+adiosulises@users.noreply.github.com>
2023-05-09 22:41:49 -0700

    login arreglado
    putos

--- src/page/Login.js
+++ src/page/Login.js
@@ -11,13 +11,8 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     //usamos el login del context
-<<<<<<< HEAD
     const { login} = useAuth();
      const [error, setError] = useState("");
-=======
-    const { login } = useAuth();
-    //const [error, setError] = useState("");
->>>>>>> aab2b88fc9ea2d957a7c0acd0b0e4e21685024b6
 
     const onLogin = (e) => {
         setError("");
@@ -42,95 +37,46 @@ const Login = () => {
     }
 
     return (
-<<<<<<< HEAD
-      <>
         <main>
-          <section>
-            <div>
-              <p> FocusApp </p>
-              <form>
-                <div>
-                  <label htmlFor="email-address">Email address</label>
-                  <input
-                    id="email-address"
-                    name="email"
-                    type="email"
-                    required
-                    placeholder="Email address"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
+        <section>
+            <div className='custom-w text-center mt-4'>
+                <h1> Boulder Grid </h1>
 
-                <div>
-                  <label htmlFor="password">Password</label>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-
-                <div>
-                  <button onClick={onLogin}>Login</button>
-                </div>
-              </form>
-              {error && <p>{error}</p>}
-
-              <p className="text-sm text-white text-center">
-                No account yet? <NavLink to="/signup">Sign up</NavLink>
-              </p>
-            </div>
-          </section>
-        </main>
-      </>
-    );
-=======
-        <>
-            <main>
-                <section>
-                    <div className='custom-w text-center mt-4'>
-                        <h1> Boulder Grid </h1>
-
-                        <form>
-                            <div className='mb-3'>
-                                <input className='form-control'
-                                    id="email-address"
-                                    name="email"
-                                    type="email"
-                                    required
-                                    placeholder="Correo"
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
-
-                            <div className='mb-3'>
-                                <input className='form-control'
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    placeholder="Contraseña"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-
-                            <div className='d-grid'>
-                                <button type='button' class='btn btn-primary btn-block'
-                                    onClick={onLogin}
-                                >
-                                    Iniciar sesión
-                                </button>
-                            </div>
-                        </form>
+                <form>
+                    <div className='mb-3'>
+                        <input className='form-control'
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            required
+                            placeholder="Correo"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
-                </section>
-            </main>
-        </>
-    )
->>>>>>> aab2b88fc9ea2d957a7c0acd0b0e4e21685024b6
+
+                    <div className='mb-3'>
+                        <input className='form-control'
+                            id="password"
+                            name="password"
+                            type="password"
+                            required
+                            placeholder="Contraseña"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+
+                    <div className='d-grid'>
+                        <button type='button' class='btn btn-primary btn-block'
+                            onClick={onLogin}
+                        >
+                            Iniciar sesión
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </main>
+    );
 }
 
 export default Login
